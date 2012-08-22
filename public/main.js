8db4220a2936dbfa3a471b20f38b988747fdedc7
Eduardo Bart <edub4rt@gmail.com>
2012-08-22 04:18:15 -0300

    simple table showing reports

---
+++ public/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#reports').dataTable( {
+    "bJQueryUI": true,
+    "bPaginate": false,
+    "bAutoWidth": false,
+  } );
+} );
--- web.rb
+++ web.rb
@@ -1,5 +1,59 @@
 require 'sinatra'
+require 'haml'
+require 'sass'
+require 'mongoid'
+
+configure do
+    Mongoid.load!('mongoid.yml')
+end
+
+class Report
+  include Mongoid::Document
+
+  belongs_to :user
+
+  field :ip, type: String
+  field :date, type: Time
+  field :os, type: String
+  field :graphics_vendor, type: String
+  field :graphics_renderer, type: String
+  field :graphics_version, type: String
+  field :painter_engine, type: Integer
+  field :fps, type: Integer
+  field :max_fps, type: Integer
+  field :fullscreen, type: Boolean
+  field :window_width, type: Integer
+  field :window_height, type: Integer
+  field :player_name, type: String
+  field :otserv_host, type: String
+  field :otserv_port, type: Integer
+  field :otserv_protocol, type: Integer
+  field :build_version, type: String
+  field :build_revision, type: Integer
+  field :build_commit, type: String
+  field :build_date, type: Time
+  field :display_width, type: Integer
+  field :display_height, type: Integer
+end
+
+class User
+  include Mongoid::Document
+  has_many :reports
+  field :uid, type: String
+end
+
+get '/main.css' do
+  sass :main
+end
+
+post '/report' do
+  report = Report.create(params)
+  report.date = Time.now
+  report.ip = request.ip
+  report.save()
+end
 
 get '/' do
-      "Hello, world"
+  @reports = Report.all
+  haml :index
 end
